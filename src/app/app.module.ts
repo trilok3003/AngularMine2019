302c5982a98b1362cf1b4f1a92e910155cfe35f3
Gaiasys-Trilok <trilok@gaiasys.in>
2019-08-02 13:49:04 +0530

    groupData completed task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GroupDataComponent } from './compoents/group-data/group-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupDataComponent
   ],
   imports: [
     BrowserModule,
