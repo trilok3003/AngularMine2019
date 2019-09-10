f2189a1d749fa070f64c1d0da26ac41e76e43d20
Gaiasys-Trilok <trilok@gaiasys.in>
2019-09-10 18:14:11 +0530

    implement left sidebar and close sidebar and dynamic sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
+  showSidebar: boolean = true;
+  menus = [
+    {path:'group',name: 'group'},
+    {path:'dynamic_form',name: 'dynamic form'},
+    {path: 'group_by_pipe',name: 'group by pipe'},
+    {path:'behaviour',name: 'behaviour'},
+    {path:'custom',name: 'custom'},
+    {path:'search',name: 'search'},
+    {path:'todo',name:'todo'},
+    {path:'test', name: 'test'},
+    {path:'operators', name: 'operators'},
+    {path: 'rxjs', name: 'rxjs'},
+    {path:'subject', name: 'subject'},
+    {path: 'async', name: 'async'},
+    {path: 'sidebar', name: 'sidebar'}
+  ]
   constructor() { }
 
   ngOnInit() {
+  }
+  getShowHide() {
+    return this.showSidebar = !this.showSidebar
   }
 
 }
