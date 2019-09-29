5e6f1606c06c8c670f29584d1e5d548ee1f5f2c2
Gaiasys-Trilok <trilok@gaiasys.in>
2019-09-29 12:09:01 +0530

    autoComplete Done

--- src/app/auto-complete/auto-complete.component.ts
+++ src/app/auto-complete/auto-complete.component.ts
@@ -5,7 +5,7 @@ import { startWith, map, endWith } from 'rxjs/operators';
  
 export interface State {
   name: string,
-  population: string
+  key: string
 }
 
 @Component({
@@ -17,19 +17,156 @@ export class AutoCompleteComponent implements OnInit {
   stateCtrl = new FormControl();
   filterStates: Observable<State[]>;
   states: State[] = [
-    {name: 'Up', population: '2 crore'},
-    {name: 'Delhi', population: '3 crore'},
-    {name: 'Mp', population: '12 crore'},
-    {name: 'Bihar', population: '9 crore'},
-    {name: 'Hariyana', population: '4 crore'},
-    {name: 'Punjab', population: '8 crore'},
-    {name: 'Rajstan', population: '5 crore'},
-    {name: 'Odisa', population: '1 crore'}
-  ]
+    {
+    "key": "AN",
+    "name": "Andaman and Nicobar Islands"
+    },
+    {
+    "key": "AP",
+    "name": "Andhra Pradesh"
+    },
+    {
+    "key": "AR",
+    "name": "Arunachal Pradesh"
+    },
+    {
+    "key": "AS",
+    "name": "Assam"
+    },
+    {
+    "key": "BR",
+    "name": "Bihar"
+    },
+    {
+    "key": "CG",
+    "name": "Chandigarh"
+    },
+    {
+    "key": "CH",
+    "name": "Chhattisgarh"
+    },
+    {
+    "key": "DH",
+    "name": "Dadra and Nagar Haveli"
+    },
+    {
+    "key": "DD",
+    "name": "Daman and Diu"
+    },
+    {
+    "key": "DL",
+    "name": "Delhi"
+    },
+    {
+    "key": "GA",
+    "name": "Goa"
+    },
+    {
+    "key": "GJ",
+    "name": "Gujarat"
+    },
+    {
+    "key": "HR",
+    "name": "Haryana"
+    },
+    {
+    "key": "HP",
+    "name": "Himachal Pradesh"
+    },
+    {
+    "key": "JK",
+    "name": "Jammu and Kashmir"
+    },
+    {
+    "key": "JH",
+    "name": "Jharkhand"
+    },
+    {
+    "key": "KA",
+    "name": "Karnataka"
+    },
+    {
+    "key": "KL",
+    "name": "Kerala"
+    },
+    {
+    "key": "LD",
+    "name": "Lakshadweep"
+    },
+    {
+    "key": "MP",
+    "name": "Madhya Pradesh"
+    },
+    {
+    "key": "MH",
+    "name": "Maharashtra"
+    },
+    {
+    "key": "MN",
+    "name": "Manipur"
+    },
+    {
+    "key": "ML",
+    "name": "Meghalaya"
+    },
+    {
+    "key": "MZ",
+    "name": "Mizoram"
+    },
+    {
+    "key": "NL",
+    "name": "Nagaland"
+    },
+    {
+    "key": "OR",
+    "name": "Odisha"
+    },
+    {
+    "key": "PY",
+    "name": "Puducherry"
+    },
+    {
+    "key": "PB",
+    "name": "Punjab"
+    },
+    {
+    "key": "RJ",
+    "name": "Rajasthan"
+    },
+    {
+    "key": "SK",
+    "name": "Sikkim"
+    },
+    {
+    "key": "TN",
+    "name": "Tamil Nadu"
+    },
+    {
+    "key": "TS",
+    "name": "Telangana"
+    },
+    {
+    "key": "TR",
+    "name": "Tripura"
+    },
+    {
+    "key": "UK",
+    "name": "Uttar Pradesh"
+    },
+    {
+    "key": "UP",
+    "name": "Uttarakhand"
+    },
+    {
+    "key": "WB",
+    "name": "West Bengal"
+    }
+    ]
   constructor() { 
     this.filterStates = this.stateCtrl.valueChanges.pipe(
       startWith(''),
-      map(state => state ? this._filterStates(state) : this.states.slice())
+    //  map(state => state ? this._filterStates(state) : this.states.slice())
+       map(state => state ? this._filterStates(state) : [])
     )
   }
 
