b949360316a4edd430b3b932deeb8efbf1802437
Gaiasys-Trilok <trilok@gaiasys.in>
2019-11-30 19:44:54 +0530

    implements Route Animations by import file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,32 +10,34 @@ import {
   group,
   animateChild
 } from '@angular/animations';
+import { routeAnimation } from './route-animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('myAnimation', [
-      transition('* => *', [
-        query(
-          ':enter',
-          [style({ opacity: 0 })],
-          { optional: true }
-        ),
-        query(
-          ':leave',
-           [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
-          { optional: true }
-        ),
-        query(
-          ':enter',
-          [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
-          { optional: true }
-        )
-      ])
-    ])
-      ] 
+  animations: [routeAnimation]
+  // animations: [
+  //   trigger('myAnimation', [
+  //     transition('* => *', [
+  //       query(
+  //         ':enter',
+  //         [style({ opacity: 0 })],
+  //         { optional: true }
+  //       ),
+  //       query(
+  //         ':leave',
+  //          [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+  //         { optional: true }
+  //       ),
+  //       query(
+  //         ':enter',
+  //         [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+  //         { optional: true }
+  //       )
+  //     ])
+  //   ])
+  //     ] 
 })
 export class AppComponent {
   title = 'AngularMine2019';
---
+++ src/app/route-animation.ts
@@ -0,0 +1,31 @@
+import {
+    transition,
+    trigger,
+    query,
+    style,
+    animate,
+    group,
+    animateChild
+  } from '@angular/animations';
+
+  export const routeAnimation =   [
+    trigger('myAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [style({ opacity: 0 })],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+           [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+          { optional: true }
+        )
+      ])
+    ])
+      ] 
