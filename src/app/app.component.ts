f8f1dfc63f0e38eadccd61f53071b7f43433e71c
Gaiasys-Trilok <trilok@gaiasys.in>
2019-11-30 19:40:28 +0530

    implements Route Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
 import { GlobalService } from './global.service';
 import { MomentService } from './moment.service';
+import {
+  transition,
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [style({ opacity: 0 })],
+          { optional: true }
+        ),
+        query(
+          ':leave',
+           [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0 }))],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1 }))],
+          { optional: true }
+        )
+      ])
+    ])
+      ] 
 })
 export class AppComponent {
   title = 'AngularMine2019';
